8e4af08d2e0e2ff37222102fa6c9b981988041dd
karl-denby <karl.denby@gmail.com>
2025-03-03 15:15:06 +0000

    add relational migrator

--- free/cluster.tf
+++ free/cluster.tf
@@ -55,3 +55,9 @@ resource "mongodbatlas_advanced_cluster" "cluster" {
 output "connection_strings" {
   value = mongodbatlas_advanced_cluster.cluster.connection_strings[0].standard_srv
 }
+
+resource "mongodbatlas_project_ip_access_list" "ip" {
+  project_id = mongodbatlas_project.free-atlas-project.id
+  ip_address = local.atlas_pub_ip_of_terraform_machine_calling_atlas_control_plane_with_api_key
+  comment    = "created via terraform"
+}
